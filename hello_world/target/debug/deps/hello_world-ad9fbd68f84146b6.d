9d551b42b990fbdc0587b3762a7d533fdd6573da
janekrobel <robelkowo@gmail.com>
2022-12-10 19:08:24 +0100

    project start
    hello_world in rust

---
+++ hello_world/target/debug/deps/hello_world-ad9fbd68f84146b6.d
@@ -0,0 +1,5 @@
+/Users/janrobel/Documents/GitHub/blockchain-project/hello_world/target/debug/deps/hello_world-ad9fbd68f84146b6.rmeta: src/main.rs
+
+/Users/janrobel/Documents/GitHub/blockchain-project/hello_world/target/debug/deps/hello_world-ad9fbd68f84146b6.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/deps/hello_world-b8c728b2d8d38e38.d
@@ -0,0 +1,5 @@
+/Users/janrobel/Documents/GitHub/blockchain-project/hello_world/target/debug/deps/hello_world-b8c728b2d8d38e38.rmeta: src/main.rs
+
+/Users/janrobel/Documents/GitHub/blockchain-project/hello_world/target/debug/deps/hello_world-b8c728b2d8d38e38.d: src/main.rs
+
+src/main.rs:
